d87537814d5b09464769f1f469ef8ed59eb81211
Miri <miril@uark.edu>
2025-06-10 10:15:53 -0500

    need to fix this import

--- App/run_detector.py
+++ App/run_detector.py
@@ -1,7 +1,7 @@
 import sys
 
 from GMMDetector import MaterialDetector
-from demo.demo_functions import visualise_flakes
+# from demo.demo_functions import visualise_flakes
 
 def run_algorithm():
     pass
